6aee3be6a795440b2dc1e2d8d6848a2930065413
Hong Phuc Le <72754942+lhplhp10102004@users.noreply.github.com>
2025-06-26 13:43:06 +0700

    chore: fix Paginated Response example

--- backend/app/model/paginated_response.go
+++ backend/app/model/paginated_response.go
@@ -1,7 +1,7 @@
 package model
 
 type PaginatedResponse[T any] struct {
-	Data     []T `json:"data" example:"[{"id":220000,"name":"Le Hong Phuc","email":"hongphucle1010@gmail.com","dob":"2004-10-10T00:00:00Z","gpa":4.0}]"`
+	Data     []T `json:"data" example:"[{\"id\":220000,\"name\":\"Le Hong Phuc\",\"email\":\"hongphucle1010@gmail.com\",\"dob\":\"2004-10-10T00:00:00Z\",\"gpa\":4.0}]"`
 	Total    int `json:"total" example:"1"`
 	Page     int `json:"page" example:"1"`
 	PageSize int `json:"page_size" example:"10"`
